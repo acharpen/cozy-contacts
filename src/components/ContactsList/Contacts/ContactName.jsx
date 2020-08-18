97fc4e9716bf3a554b3316ee099789e3954a7ad2
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-08-18 17:30:12 +0200

    fix: Use displayName to show contact's name

--- src/components/ContactsList/Contacts/ContactName.jsx
+++ src/components/ContactsList/Contacts/ContactName.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ContactName = ({ firstname, lastname }) => (
-  <div className="u-ellipsis">
-    <span className="contact-firstname">{firstname}</span>
-    &nbsp;
-    <span className="contact-lastname">{lastname}</span>
-  </div>
-)
+const ContactName = ({ displayName, lastname }) => {
+  const namesToDisplay = (displayName && displayName.split(' ')) || []
+  return (
+    <div className="u-ellipsis">
+      {namesToDisplay.map((name, key) => (
+        <span key={`display-${key}`}>
+          <span className={name === lastname ? 'contact-lastname' : 'u-ml-1'}>
+            {name}
+          </span>
+          &nbsp;
+        </span>
+      ))}
+    </div>
+  )
+}
+
 ContactName.propTypes = {
-  firstname: PropTypes.string,
-  lastname: PropTypes.string,
-  me: PropTypes.bool
+  displayName: PropTypes.string,
+  lastname: PropTypes.string
 }
 ContactName.defaultProps = {
-  firstname: '',
-  lastname: '',
-  me: false
+  displayName: ''
 }
+
 export default ContactName
